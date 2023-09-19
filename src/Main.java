5dbd856f6ab5d8e05e84bef1f9a555472aad70c1
Artem.don <dontsovki@gmail.com>
2023-09-19 16:16:41 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import progect.heaps;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1234, 4, 7, 2, 1, -3, 0, 567, 45, 90, 34,2, 234};
+
+        new heaps(arr);
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
     }
 }
